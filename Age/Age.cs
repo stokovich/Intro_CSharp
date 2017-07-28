43fc80e8de8a72be48a3c724cef949e191ab15d4
Stoyan Penchev <spenchev@gmail.com>
2017-07-28 08:57:42 +0300

    after Age task

---
+++ Age/Age.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Age
+{
+    static void Main()
+    {
+        DateTime birthday = DateTime.ParseExact(Console.ReadLine(),"MM.dd.yyyy", null);
+        DateTime currentday = DateTime.Today;
+        int age = currentday.Year - birthday.Year;
+        if (birthday > currentday.AddYears(-age))
+        {
+            age--;
+        }
+        Console.WriteLine(age);
+        Console.WriteLine(age + 10);
+    }
+}
