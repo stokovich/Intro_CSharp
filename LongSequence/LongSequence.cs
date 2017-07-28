cb4a45baa03844fb03a22151bc2ff5fcd755e531
Stoyan Penchev <spenchev@gmail.com>
2017-07-28 09:09:15 +0300

    after long sequence task

--- LongSequence/LongSequence.cs
+++ LongSequence/LongSequence.cs
@@ -4,6 +4,19 @@ class LongSequence
 {
     static void Main()
     {
-
+        int num_member = 2;
+        for (int i = 0; i < 1000; i++)
+        {
+            if (num_member % 2 == 0)
+            {
+                Console.WriteLine(num_member);
+                num_member++;
+            }
+            else
+            {
+                Console.WriteLine(-num_member);
+                num_member++;
+            }
+        }
     }
 }
